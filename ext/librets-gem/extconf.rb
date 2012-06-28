e945d349c4e9a5ba0b7ce8f9f2c413f3ba0dc799
Fred Ngo <fredngo@gmail.com>
2012-06-28 09:43:02 -0700

    add pcre and swig

--- ext/librets-gem/extconf.rb
+++ ext/librets-gem/extconf.rb
@@ -4,42 +4,55 @@ root = File.expand_path('../../..', __FILE__)
 
 Dir.chdir(File.join(root, 'vendor')) do
   
-  software = 'boost_1_49_0'
-  #system "bunzip2 -k #{software}.tar.bz2"
-  #system "tar xvf #{software}.tar"
-  #Dir.chdir(File.join(root, "vendor/#{software}")) do
-  #  args = ["--prefix=#{root}", '--with-libraries=filesystem,program_options']
-  #  system './bootstrap.sh', *args
-  #  args = ['install', "--prefix=#{root}", 'threading=multi', '--layout=tagged',
-  #          '--with-filesystem', '--with-program_options'] 
-  #  system './b2', *args
-  #  system './b2', '--clean'
-  #end
-  #system "rm -f #{software}.tar"
-  #system "rm -rf #{software}"
-  
-  # Need SWIG??
-  
-  software = 'librets-1.5.3'
-  #system "bunzip2 #{software}.tar.bz2"
-  #system "tar xvf #{software}.tar"
-  #Dir.chdir(File.join(root, "vendor/#{software}")) do
-  #  args = ['--disable-debug',
-  #          '--enable-shared_dependencies',
-  #          "--prefix=#{root}",
-  #          "--with-boost-prefix=#{root}"
-  #          '--disable-dotnet',
-  #          '--disable-java',
-  #          '--disable-perl',
-  #          '--disable-php',
-  #          '--disable-python']
-  #  system './configure', *args
-  #  system 'make'
-  #  system 'make install'
-  #  system 'make clean'
-  #end
-  #system "rm -f #{software}.tar"
-  #system "rm -rf #{software}"  
+  ['boost_1_49_0', 'swig-2.0.7', 'librets-1.5.3'].each do |package|
+
+    #system "bunzip2 -k #{package}.tar.bz2"
+    #system "tar xvf #{package}.tar"
+    Dir.chdir(File.join(root, "vendor/#{package}")) do
+      case package
+      when 'boost_1_49_0'    
+      #  args = ["--prefix=#{root}", '--with-libraries=filesystem,program_options']
+      #  system './bootstrap.sh', *args
+      #  args = ['install', "--prefix=#{root}", 'threading=multi', '--layout=tagged',
+      #          '--with-filesystem', '--with-program_options'] 
+      #  system './b2', *args
+      #  system './b2', '--clean'
+      
+      when 'swig-2.0.7'
+      #  system 'mv ../pcre-8.30.tar.gz .'
+      #  system 'Tools/pcre-build.sh'
+      #  args = ["--prefix=#{root}", "--with-boost=#{root}"] #, "--with-pcre-prefix=#{root}", "--with-pcre-exec-prefix=#{root}/bin"]
+      #  system './configure', *args
+      #  system 'make'
+      #  system 'make install'
+      #  system 'make clean'
+     
+      when 'librets-1.5.3'
+      #system "PATH=$PATH:#{root}/bin; export PATH"
+      #  args = ["--prefix=#{root}",
+      #          "--with-boost-prefix=#{root}",
+      #          '--enable-shared_dependencies', '--enable-depends'
+      #          '--disable-java', '--disable-perl', '--disable-python']
+      #  system './configure', *args
+      #  system 'make'
+      
+      #  system "sed -e 's|prefix = \$(DESTDIR)/usr/local|prefix = #{root}|' Makefile > Makefile"
+
+      #  system 'make install'
+      #  system "mv #{root}/lib/ruby/site_ruby/1.9.1/x86_64-linux/librets_native.so #{root}/lib"
+      #  system "mv #{root}/lib/ruby/site_ruby/1.9.1/librets.rb #{root}/lib"
+      #  system "rm -rf #{root}/lib/ruby"
+
+      #  system 'make clean'
+
+
+
+
+    end
+    #system "rm -f #{package}.tar"
+    #system "rm -rf #{package}" 
+    
+  end
 end
 
 create_makefile 'librets-gem'
